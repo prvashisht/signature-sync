c5f63c2a234664875c52c211124d1474323cd7e3
Pratyush Vashisht <pratyushvashisht1@gmail.com>
2021-12-18 20:24:07 +0530

    Make Extension workable again
    - Remove Slider enabler to simplify code
    - Change chrome.storage to generic browser.storage
    - Change sync storage back to local storage (not working in Firefox)
    - Remove contentstyle.css used for slider

--- contentScript.js
+++ contentScript.js
@@ -1,84 +1,12 @@
-document.addEventListener('focus', function (event) {
-  var activeElement = document.activeElement,
-    isMessageBox = activeElement.getAttribute('name') === 'message' &&
-      activeElement.classList.contains('ember-text-area') &&
-      activeElement.tagName.toLowerCase() === 'textarea',
-    messagebox;
-  if (isMessageBox && !activeElement.value) {
-    chrome.storage.sync.get(['linkedinsignature'], function (item) {
-      var parsedLISignature = JSON.parse(item.linkedinsignature)
-      if (parsedLISignature.enabled) {
-        activeElement.value = parsedLISignature.text;
-        _setCaretPosition(activeElement, 0);
-      }
-      return;
-    });
-  }
-}, true);
-
-document.addEventListener('DOMNodeInserted', function (e) {
-  var node = e.target,
-    classList = node.classList;
-
-  if (classList &&
-    (classList.contains('msg-form__left-actions') ||
-      classList.contains('msg-messaging-form__left-actions') ||
-      classList.contains('msg-compose-form__left-actions'))) {
-    chrome.storage.sync.get(['linkedinsignature'], function (item) {
-      if (!item.linkedinsignature) {
-        var signdetails = {
-          enabled: true,
-          text: "Regards"
-        }
-        chrome.storage.sync.set({ 'linkedinsignature': JSON.stringify(signdetails) }, function () {
-          _doOurLIStuff(node, signdetails)
-        });
-      } else {
-        _doOurLIStuff(node, JSON.parse(item.linkedinsignature));
-        return;
-      }
-    });
-  }
-});
-
-function _doOurLIStuff (node, parsedLISignature) {
-  var sliderChecked = parsedLISignature.enabled ? 'checked' : '',
-    tooltipTextVar = '',
-    tooltipClass = '';
-  var eleToAdd = document.createElement('label');
-  eleToAdd.classList.add('lnkdmsg_switch');
-  if (!parsedLISignature.sliderUsedBefore) {
-    tooltipTextVar = '<span class="lnkdmsg_tooltiptext">This switch toggles the LinkedIn Messages Signatures on/off.<br />Once you use this switch, the tooltip will disappear.</span>';
-    eleToAdd.classList.add('lnkdmsg_tooltip');
-  }
-  eleToAdd.innerHTML = `
-    <input class="lnkdmsg_checkbox" type="checkbox" ${sliderChecked}>
-    <span class="lnkdmsg_slider lnkdmsg_round"></span>
-    ${tooltipTextVar}
-  `;
-  node.appendChild(eleToAdd);
-
-  eleToAdd.addEventListener('click', function (e) {
-    if (!parsedLISignature.sliderUsedBefore) {
-      var li_signTooltips = document.getElementsByClassName('lnkdmsg_tooltip');
-      for (var i = li_signTooltips.length - 1; i >= 0; i--) {
-        li_signTooltips[i].classList.remove('lnkdmsg_tooltip')
-      }
-      var li_signTooltipTexts = document.getElementsByClassName('lnkdmsg_tooltiptext');
-      for (var i = li_signTooltipTexts.length - 1; i >= 0; i--) {
-        li_signTooltipTexts[i].parentNode.removeChild(li_signTooltipTexts[i]);
-      }
-    }
-    _changeSignatureEnableState({
-      enabled: e.target.checked,
-      text: parsedLISignature.text,
-      sliderUsedBefore: parsedLISignature.sliderUsedBefore || true
-    });
-  });
-  return;
+// Every line should be a paragraph. 
+// Including empty lines which should be a whitespace + break tag inside a paragraph
+const modifySignatureToHTML = signature => {
+  return "<p> <br/></p>" + signature.split('\n').map(signPart => {
+    return `<p>${signPart || " <br/>"}</p>`
+  }).join(" ")
 }
 
-function _setCaretPosition (elem, caretPos) {
+const _setCaretPosition = (elem, caretPos) => {
   if (elem != null) {
     if (elem.createTextRange) {
       var range = elem.createTextRange();
@@ -95,15 +23,19 @@ function _setCaretPosition (elem, caretPos) {
   }
 }
 
-function _changeSignatureEnableState (signdetails) {
-  chrome.storage.sync.set({ 'linkedinsignature': JSON.stringify(signdetails) }, function () {
-    var li_signcheckboxes = document.getElementsByClassName('lnkdmsg_checkbox');
-    for (var i = li_signcheckboxes.length - 1; i >= 0; i--) {
-      if (signdetails.enabled) {
-        li_signcheckboxes[i].setAttribute('checked', 'checked');
-      } else {
-        li_signcheckboxes[i].removeAttribute("checked");
+// Add signature when message box is focused
+document.addEventListener('focus', function (event) {
+  var activeElement = document.activeElement,
+    isMessageBox = activeElement.classList.contains('msg-form__contenteditable');
+
+  if (isMessageBox && !activeElement.textContent.trim()) {
+    browser.storage.local.get(['linkedinsignature'], function (item) {
+      if (item.linkedinsignature.enabled) {
+        activeElement.innerHTML = modifySignatureToHTML(item.linkedinsignature.text);
+        _setCaretPosition(activeElement, 0);
+        activeElement.click();
       }
-    }
-  });
-}
+      return;
+    });
+  }
+}, true);
--- popup.js
+++ popup.js
@@ -2,35 +2,39 @@ document.addEventListener('DOMContentLoaded', function () {
   var signatureTextDiv = document.getElementById('signature'),
     signatureEnableBox = document.getElementById('enablesign')
   signatureTextDiv.disabled = true;
+
   document.getElementById('editSignature').addEventListener('click', function () {
     signatureTextDiv.disabled = false;
     signatureTextDiv.focus();
   });
+
   document.getElementById('saveSignature').addEventListener('click', function () {
     var sign = signatureTextDiv.value,
       enablesign = signatureEnableBox.checked,
-      signdetails = {
+      linkedinsignature = {
         enabled: enablesign,
         text: sign
       }
-    chrome.storage.sync.set({ 'linkedinsignature': JSON.stringify(signdetails) }, function () {
+    browser.storage.local.set({ linkedinsignature }, () => {
       signatureTextDiv.disabled = true;
     });
   });
-  chrome.storage.sync.get(['linkedinsignature'], function (item) {
-    if (!item.linkedinsignature) {
-      var signdetails = {
+
+  browser.storage.local.get('linkedinsignature', function (item) {
+    if (!item || !item.linkedinsignature) {
+      var linkedinsignature = {
         enabled: true,
         text: "Regards"
       }
-      chrome.storage.sync.set({ 'linkedinsignature': JSON.stringify(signdetails) }, function () {
+      browser.storage.local.set({ linkedinsignature }, () => {
         signatureTextDiv.value = "Regards";
         signatureEnableBox.checked = true;
         signatureTextDiv.disabled = true;
       });
+    } else {
+      var signdetails = item.linkedinsignature;
+      signatureTextDiv.value = signdetails.text;
+      signatureEnableBox.checked = signdetails.enabled;
     }
-    var signdetails = JSON.parse(item.linkedinsignature);
-    signatureTextDiv.value = signdetails.text;
-    signatureEnableBox.checked = signdetails.enabled;
   })
 });
