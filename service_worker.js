422d2628a47351a6f16b3fbf5fa29754d850a566
Pratyush Vashisht <4387947+prvashisht@users.noreply.github.com>
2023-10-10 14:59:48 +0200

    Fill debug data to uninstall survey automatically
    Also bumped the version number

--- service_worker.js
+++ service_worker.js
@@ -10,4 +10,30 @@ chrome.runtime.onMessage.addListener((request) => {
     }
 });
 
-chrome.runtime.setUninstallURL('https://forms.gle/mi2935LFqWekbVyb9');
+chrome.runtime.onInstalled.addListener(installInfo => {
+    let installDate, updateDate;
+    if (installInfo.reason === "install") {
+        installDate = new Date();
+    } else {
+        updateDate = new Date().toISOString();
+    }
+    chrome.runtime.getPlatformInfo(platformInfo => {
+        let debugData = {
+            ...platformInfo,
+            agent: navigator.userAgent,
+            locale: navigator.language,
+            platform: navigator.platform,
+            timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        }
+        if (installDate) debugData.installDate = installDate;
+        if (updateDate) debugData.updateDate = updateDate;
+
+        console.log(debugData);
+        const encodedTechnicalDetails = encodeURIComponent(
+            Object.keys(debugData)
+            .map(debugKey => `${debugKey}: ${debugData[debugKey]}`)
+            .join("\n")
+        );
+        chrome.runtime.setUninstallURL(`https://docs.google.com/forms/d/e/1FAIpQLSfVQNeGxn0pb6CnG1WRjeWPWjheTa30a7L58g7B7X-x8jaIqg/viewform?usp=pp_url&entry.1303663358=${encodedTechnicalDetails}`);
+    });
+});
